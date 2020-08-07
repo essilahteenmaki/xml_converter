d6a79d739d01f15806d433342c9d0106e2cdfcb0
Essi Lähteenmäki <lahteenmakiessi@hotmail.fi>
2020-08-07 13:28:16 +0300

    refactoring and fixes

--- src/main/java/com/parser/dataparser/services/Parser.java
+++ src/main/java/com/parser/dataparser/services/Parser.java
@@ -20,52 +20,41 @@ import org.w3c.dom.NodeList;
 
 public class Parser {
 	
-	public static void parse() {
+	public static void parseXml() {
 
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		try {			
-		DocumentBuilder builder = factory.newDocumentBuilder();
-		File file = createFile();
-		Document document = builder.parse(file);
-		//test(document);
-		test2(document);		
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			File file = createFile();
+			Document document = builder.parse(file);
+			createObjects(document);		
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 		
 	}
 	
-
-	public static void test (Document doc ) {
-		List<Premise> premises = new ArrayList<Premise>();		
-        Element element = doc.getDocumentElement();        
-        NodeList nodeList = element.getElementsByTagName("item");
-        
-        for (int i = 0; i<nodeList.getLength(); i++) {
-        	Node node = nodeList.item(i);
-            Element nodeElement = (Element) node;
-    		NodeList nL = nodeElement.getElementsByTagName("property");
-    		String address = nL.item(3).getTextContent();
-    		int id = Integer.parseInt(nL.item(1).getTextContent().trim());
-    		String city = nL.item(5).getTextContent();
-    		String m2 = nL.item(10).getTextContent();
-    		/*
-    		int temp = nL.item(24).getChildNodes().getLength();
-    		for (int j = 0; j < temp; j++) {
-    			String premisetype = nL.item(24).getChildNodes().item(j).getLastChild().getTextContent();
-    		}
-    		*/
-			premises.add(new Premise(id, address, city, m2));
-        }
-
-		for (Premise premise : premises) {
-			System.out.println(premise.toString());
+	public static File createFile() throws Exception {
+		final String address = "LINK TO XML/XHTML";
+		URL url = new URL(address);
+		File xmlFile = File.createTempFile("tempxmlfile", null); 
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+		OutputStream os = new FileOutputStream(xmlFile);
+		String line;
+		
+		while ((line = br.readLine()) != null) {
+			os.write(line.getBytes());
 		}
-        		
-	}
+		os.close();
+		br.close();
+		
+		xmlFile.deleteOnExit();
+		return xmlFile;
+	}	
 	
-	public static void test2 (Document doc ) {
-		List<Premise> premises = new ArrayList<Premise>();		
+
+	public static void createObjects (Document doc ) {		
         Element element = doc.getDocumentElement();        
         NodeList items = element.getElementsByTagName("item");
         
@@ -73,92 +62,92 @@ public class Parser {
 			Node premiseNode = items.item(i);
 			Element premise = (Element) premiseNode;
 			NodeList properties = premise.getElementsByTagName("property"); 
-					
-			for (int j=0; j < properties.getLength(); j++) {
-				//System.out.println(properties.getLength());
-				Node propertyNode = properties.item(j);
-				Element property = (Element) propertyNode;
-				String address = getValue(property, "street");
-				String city = getValue(property, "town");
-				String m2 = getValue(property, "totalaream2");
-				String premisetype = getSubvalues(property, "realtytypeoption2");
-				String imglink = getSubvalues(property, "image");
-				//Integer id = Integer.parseInt(getValue(property, "cust_itemcode"));
-				
-				try {
-					/*
-					if((property.getAttributes().getNamedItem("name").getTextContent()).equals("street")) {
-						String street = property.getTextContent();
-						System.out.println(street);
-					}
-					
-					if((property.getAttributes().getNamedItem("name").getTextContent()).equals("town")) {
-						String city = property.getTextContent();
-						System.out.println(city);
-					}
-					
-					if((property.getAttributes().getNamedItem("name").getTextContent()).equals("totalaream2")) {
-						String m2 = property.getTextContent();
-						System.out.println(m2);
-					}
-					
-					if((property.getAttributes().getNamedItem("name").getTextContent()).equals("cust_itemcode")) {
-						Integer id = Integer.parseInt(property.getTextContent().trim());
-						System.out.println(id);
-					}
-					
-					
-					
-					if((property.getAttributes().getNamedItem("name").getTextContent()).equals("realtytypeoption2")) {
-						StringBuilder temp = new StringBuilder();
-						NodeList types = property.getChildNodes();
-						for (int k=0; k<types.getLength(); k++) {
-							temp.append(types.item(k).getFirstChild().getTextContent() + " ");
-						}
-						String premisetype = temp.toString();
-						System.out.println(premisetype);
-					}
-					
-*/
-					if((property.getAttributes().getNamedItem("name").getTextContent()).equals("text")) {
-						NodeList textproperties = property.getChildNodes();
-						String premisedescription = textproperties.item(1).getChildNodes().item(1).getTextContent();
-						System.out.println("txtprops: " + textproperties.getLength());
-						System.out.println("txtprops: " + property.getTextContent());
-					}
-	/*				
-					if((property.getAttributes().getNamedItem("name").getTextContent()).equals("image")) {
-						StringBuilder temp = new StringBuilder();
-						NodeList imgproperties = property.getChildNodes();
-						for (int l=0; l< imgproperties.getLength(); l++) {
-							temp.append(imgproperties.item(l).getLastChild().getTextContent() + ",");
-						}
-						String images = temp.toString();
-						System.out.println(images);
-					}
-					
-	*/				
-					
-				} catch (NullPointerException ne) {
-					ne.printStackTrace();
-				}					
-			}
+			makeObjects(properties);
 						
-		}
- 
-        
-/*
-
-
-		for (Premise premise : premises) {
-			System.out.println(premise.toString());
-		}
-*/    		
+		}           		
 	}
 	
+	public static String getString(Element property) {
+		String value = "";
+		if (property.getTextContent() != null) {
+			value = property.getTextContent();
+		}
+		System.out.println(value);
+		return value;
+	}
+	
+	
+	public static void makeObjects(NodeList properties) {
+		List<Premise> premises = new ArrayList<Premise>();
+		String value = "";
+		String street = "";
+		String city = "";
+		String id = "";
+		String m2 = "";
+		String premisetype = "";
+		String imglink = "";
+		String premisedescription = "";
+		
+		for (int j=0; j < properties.getLength(); j++) {
+			Node propertyNode = properties.item(j);
+			Element property = (Element) propertyNode;
+			
+			if (property.hasAttributes()) {
+				if((property.getAttributes().getNamedItem("name").getTextContent()).equals("street")) {
+					street = getString(property);
+				}
+				
+				if((property.getAttributes().getNamedItem("name").getTextContent()).equals("town")) {
+					city = getString(property);
+				}
+				
+				if((property.getAttributes().getNamedItem("name").getTextContent()).equals("totalaream2")) {
+					m2 = getString(property);
+				}
+				
+				if((property.getAttributes().getNamedItem("name").getTextContent()).equals("cust_itemcode")) {
+					id = getString(property);
+				}
+							
+				
+				if((property.getAttributes().getNamedItem("name").getTextContent()).equals("realtytypeoption2")) {
+					StringBuilder temp = new StringBuilder();
+					NodeList types = property.getChildNodes();
+					for (int k=0; k<types.getLength(); k++) {
+						temp.append(types.item(k).getFirstChild().getTextContent() + " ");
+					}
+					premisetype = temp.toString();
+					System.out.println(premisetype);
+				}
+				
+	/*
+				if((property.getAttributes().getNamedItem("name").getTextContent()).equals("text")) {
+					NodeList textproperties = property.getChildNodes();
+					String premisedescription = textproperties.item(1).getChildNodes().item(1).getTextContent();
+					System.out.println("txtprops: " + textproperties.getLength());
+					System.out.println("txtprops: " + property.getTextContent());
+				}
+	*/			
+					
+				if((property.getAttributes().getNamedItem("name").getTextContent()).equals("image")) {
+					StringBuilder temp = new StringBuilder();
+					NodeList imgproperties = property.getChildNodes();
+					for (int l=0; l< imgproperties.getLength(); l++) {
+						temp.append(imgproperties.item(l).getLastChild().getTextContent() + ",");
+					}
+					String images = temp.toString();
+					System.out.println(images);
+				}
+			}			
+			
+			// make new premise with values		, id to int	
+		}		
+	}
+	
+	
+//delete	
 	public static String getValue(Element property, String item) {
 		String value = "";		
-		if (property != null) {
 			try {
 				if((property.getAttributes().getNamedItem("name").getTextContent()).equals(item)) {
 					String tempvalue = property.getTextContent().trim();
@@ -169,7 +158,7 @@ public class Parser {
 			} catch (NullPointerException ex) {
 				ex.printStackTrace();
 			}
-		}
+		
 		return value;
 	}
 	
@@ -190,32 +179,13 @@ public class Parser {
 						temp.append(subproperties.item(l).getLastChild().getTextContent() + ",");
 					}					
 				}
-			
+				
+											
 			value = temp.toString();
 			System.out.println(value);
 		}
 		return value;
 	}
 	
-	
-	
-	//w. java.io
-	public static File createFile() throws Exception {
-		final String address = "xx";
-		URL url = new URL(address);
-		File xmlFile = File.createTempFile("tempxmlfile", null); 
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
-		OutputStream os = new FileOutputStream(xmlFile);
-		String line;
-		
-		while ((line = br.readLine()) != null) {
-			os.write(line.getBytes());
-		}
-		os.close();
-		br.close();
-		
-		return xmlFile;
-	}	
 	
 }
--- src/main/java/com/parser/dataparser/web/AppController.java
+++ src/main/java/com/parser/dataparser/web/AppController.java
@@ -10,7 +10,7 @@ public class AppController {
 	
 	@GetMapping("/test2")
 	public static void convert() {
-		Parser.parse();
+		Parser.parseXml();
 	}
 	
 	
